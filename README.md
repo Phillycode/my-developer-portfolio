5b7b2e3ff0e722647f4a7d80adc9668da762b55c
Philip Bergh <pjbergh@gmail.com>
2025-08-14 21:51:28 +0200

    Add additional tests and update README

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 
 This is a simple but clean sticky notes web application created for the purposes of a bootcamp Django project and is only intended for private use and learning purposes. The application enables all of CRUD options for sticky notes and displays them in a user-friendly manner by making use of Bootstrap's components. Due to the nature of sticky notes and how I personally like to use them, I opted for a single-user application with no requirement for an admin user. The Note model also has an optional title, meaning that you can simply put in a description for the sticky note if it's a reminder or quick note. This helped to keep it simple and is everything that I would like in a sticky note application as it allows for adding and checking quick reminders and removing them as they are completed.
 
-Latest update: Added delete button in the note_list view
+Latest update: Added additional tests in tests.py to cover all use cases.
 
 # Setup instructions
 
 - Create and activate a virtual environment (venv)
 - Then install the dependencies from the included requirements.txt file:
 
+```bash
 pip install -r requirements.txt
+```
 
 - Apply database migrations and run the server:
 
+```bash
 python manage.py migrate
 python manage.py runserver
+```
 
 - Open your browser at http://127.0.0.1:8000
 
@@ -22,7 +26,9 @@ python manage.py runserver
 
 - You can run tests for this app by navigating to the sticky_notes project folder in your terminal and run:
 
+```bash
 python manage.py test notes
+```
 
 # Credits and References
 
--- sticky_notes/notes/tests.py
+++ sticky_notes/notes/tests.py
@@ -5,9 +5,9 @@ from .models import Note
 
 # Tests here
 class NoteModelTest(TestCase):
-    """This test class tests whether our Note model functions as
-    intended and also tests 'create' and 'delete' functionality for
-    our sticky notes."""
+    """This test class tests whether our Note model works as intended
+    by being able to successfully create or delete sticky notes from
+    the database."""
 
     def setUp(self):
         # Create a Note object for initial "create" testing
@@ -48,9 +48,9 @@ class NoteModelTest(TestCase):
 
 
 class NoteViewTest(TestCase):
-    """This test class tests whether our views are working
-    correctly and is responsible for testing our 'read' and
-    'update' use cases."""
+    """This test class is responsible for testing all of our views
+    and their corresponding urls. These tests therefore include the
+    create, read, update and delete use cases of our application."""
 
     def setUp(self):
         # Create an instance variable (self.note) to hold our
@@ -73,7 +73,7 @@ class NoteViewTest(TestCase):
         self.assertContains(response, "This is a test sticky.")
 
     def test_note_update_view(self):
-        # Test updating a note
+        # Test the update_view
         update_url = reverse("note_update", args=[self.note.pk])
         response = self.client.post(
             update_url,
@@ -87,3 +87,24 @@ class NoteViewTest(TestCase):
         self.note.refresh_from_db()
         self.assertEqual(self.note.title, "Updated Sticky")
         self.assertEqual(self.note.content, "Updated content.")
+
+    def test_note_create_view(self):
+        # Test the note_create view
+        create_url = reverse("note_create")
+        data = {
+            "title": "New Test Sticky",
+            "content": "Testing the creation of a new Sticky Note.",
+        }
+        response = self.client.post(create_url, data)
+        self.assertEqual(response.status_code, 302)  # Test for redirect
+        # Use .filter instead of .get to avoid error in case the
+        # note is not found in the database.
+        self.assertTrue(Note.objects.filter(title="New Test Sticky").exists())
+
+    def test_note_delete_view(self):
+        # Test the note_delete view
+        delete_url = reverse("note_delete", args=[self.note.pk])
+        response = self.client.post(delete_url)
+        self.assertEqual(response.status_code, 302)  # Test for redirect
+        # Use .filter to return an empty result after deletion.
+        self.assertFalse(Note.objects.filter(pk=self.note.pk).exists())
